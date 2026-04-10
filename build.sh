4229a54407c12e98bd9515c0ad387b17829588c8
Paul Cowan <paul@custardsource.com>
2026-04-11 09:37:04 +1000

    Update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build . -t ghcr.io/thecowan/stackchecker
-docker push ghcr.io/thecowan/stackchecker:master
+docker build . -t ghcr.io/thecowan/stackchecker:latest
+docker push ghcr.io/thecowan/stackchecker:latest
